32a596a9d78d28703142f5d712f3dc94d81a4d80
umairJanjua <56170485+UmairJanjua6@users.noreply.github.com>
2020-10-31 11:35:13 +0500

    added 1 contact

--- app/src/main/java/com/example/assignone/ActivityContactInfo.java
+++ app/src/main/java/com/example/assignone/ActivityContactInfo.java
@@ -3,24 +3,47 @@ package com.example.assignone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class ActivityContactInfo extends AppCompatActivity {
     private Button saveInfoButton;
-    EditText userName;
-    EditText userNumber;
-    EditText userEmail;
+    EditText user_Name;
+    EditText user_Number;
+    EditText user_Email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_info);
+        user_Name = findViewById(R.id.userName);
+        user_Number = findViewById(R.id.userNumber);
+        user_Email = findViewById(R.id.userEmail);
+        saveInfoButton = findViewById(R.id.saveInfo);
+
+        saveInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = user_Name.getText().toString();
+                String number = user_Number.getText().toString();
+                String email = user_Email.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("user_name", name);
+                intent.putExtra("user_Number" , number);
+                intent.putExtra("user_Email" , email);
+                startActivity(intent);
+
+//                user_Number = findViewById(R.id.userNumber);
+//                String  nbr = user_Number.getText().toString();
+//                Intent number = new Intent(getApplicationContext(), ActivityShowContact.class);
+//                number.putExtra("user_number", nbr);
+//                startActivity(number);
+            }
+        });
 
     }
-
-
-
 }
--- app/src/main/java/com/example/assignone/MainActivity.java
+++ app/src/main/java/com/example/assignone/MainActivity.java
@@ -6,25 +6,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-
+    private Button newContact;
+    TextView text_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.addContactButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        text_view = (TextView) findViewById(R.id.txt_view);
+
+        Intent intent = getIntent();
+        String strName = intent.getStringExtra("user_name");
+        String strNumber = intent.getStringExtra("user_Number");
+        String strEmail = intent.getStringExtra("user_Email");
+        text_view.setText(strName);
+
+        newContact = findViewById(R.id.addContactButton);
+        newContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivityContactInfo();
+            }
+        });
+
+        text_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityShowContact();
+                Intent showIntent = new Intent(getApplicationContext(), ActivityShowContact.class);
+                showIntent.putExtra("user_name" , strName);
+                showIntent.putExtra("user_number", strNumber);
+                showIntent.putExtra("user_Email" , strEmail);
+                startActivity(showIntent);
             }
         });
     }
     public void openActivityContactInfo() {
         Intent intent = new Intent(this, ActivityContactInfo.class);
         startActivity(intent);
+    }
+    public void openActivityShowContact() {
+        Intent showIntent = new Intent(this, ActivityShowContact.class);
+        startActivity(showIntent);
     }
 }
